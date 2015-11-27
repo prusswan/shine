10371ab1555907df33ba229fbbf590826b908db2
prusswan <prusswan@gmail.com>
2015-11-27 18:57:51 +0800

    added customer model and updated seeds

--- Gemfile
+++ Gemfile
@@ -33,6 +33,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 # gem 'capistrano-rails', group: :development
 
 gem 'devise'
+gem 'faker'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
@@ -41,4 +42,3 @@ group :development, :test do
   # Access an IRB console on exception pages or by using <%= console %> in views
   gem 'web-console', '~> 2.0'
 end
-
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Customer.transaction do
+  350.times do |i|
+    Customer.create!(
+      first_name: Faker::Name.first_name,
+      last_name: Faker::Name.last_name,
+      username: "#{Faker::Internet.user_name}#{i}",
+      email: "#{Faker::Internet.user_name}#{i}@#{Faker::Internet.domain_name}"
+    )
+  end
+end
