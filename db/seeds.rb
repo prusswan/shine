d690cb1ce77649036ce6c7b40202f08e2ff085ad
prusswan <prusswan@gmail.com>
2015-11-30 17:14:36 +0800

    improved seeding task (commit in batches of 1000)

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,19 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Customer.transaction do
-  350.times do |i|
-    Customer.create!(
-      first_name: Faker::Name.first_name,
-      last_name: Faker::Name.last_name,
-      username: "#{Faker::Internet.user_name}#{i}",
-      email: "#{Faker::Internet.user_name}#{i}@#{Faker::Internet.domain_name}"
-    )
+350_000.times.each_slice(1000) do |slice|
+  time = Benchmark.realtime do
+    Customer.transaction do
+      slice.each do |i|
+        Customer.create!(
+          first_name: Faker::Name.first_name,
+          last_name: Faker::Name.last_name,
+          username: "#{Faker::Internet.user_name}#{i}",
+          email: "#{Faker::Internet.user_name}#{i}@#{Faker::Internet.domain_name}"
+        )
+      end
+    end
   end
+
+  p "[#{slice.first} - #{slice.last}] Time taken: #{time} seconds"
 end
