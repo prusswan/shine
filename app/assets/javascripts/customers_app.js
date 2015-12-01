a3ed343124e13110e984b16d9fed582afa1569f3
prusswan <prusswan@gmail.com>
2015-12-01 15:48:13 +0800

    fixed pagination for angular

--- app/assets/javascripts/customers_app.js
+++ app/assets/javascripts/customers_app.js
@@ -3,9 +3,16 @@ var app = angular.module('customers', []);
 app.controller("CustomerSearchController", ['$scope', '$http', function($scope, $http) {
   $scope.customers = [];
 
+  var page = 0;
+
   $scope.search = function(searchTerm) {
     $http.get("/customers.json",
-      { "params": { "keywords": searchTerm } }
+      { "params":
+        {
+          "keywords": searchTerm,
+          "page": page
+        }
+      }
     )
     .success(
       function(data,status,headers,config) {
@@ -17,5 +24,17 @@ app.controller("CustomerSearchController", ['$scope', '$http', function($scope, 
         alert("There was a problem: " + status);
       }
     );
+  }
+
+  $scope.previousPage = function() {
+    if (page > 0) {
+      page = page - 1;
+      $scope.search($scope.keywords);
+    }
+  }
+
+  $scope.nextPage = function() {
+    page = page + 1;
+    $scope.search($scope.keywords);
   }
 }]);
