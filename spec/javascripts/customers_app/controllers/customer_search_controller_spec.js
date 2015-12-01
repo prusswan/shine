24d008f683ac2ab8f2b820bfec9ec52706e1eb82
prusswan <prusswan@gmail.com>
2015-12-01 19:39:40 +0800

    jasmine spec for customer search (with ajax)

--- spec/javascripts/customers_app/controllers/customer_search_controller_spec.js
+++ spec/javascripts/customers_app/controllers/customer_search_controller_spec.js
@@ -16,4 +16,47 @@ describe("CustomerSearchController", function() {
       expect(scope.customers).toEqualData([]);
     });
   });
+
+  describe("Fetching Search Results", function() {
+    var scope = null,
+      controller = null,
+      httpBackend = null,
+      serverResults = [
+        {
+          id: 123,
+          first_name: "Bob",
+          last_name: "Jones",
+          email: "bjones@foo.net",
+          username: "jonesy"
+        },
+        {
+          id: 456,
+          first_name: "Bob",
+          last_name: "Johnsons",
+          email: "johnboy@bar.info",
+          username: "bobbyj"
+        }
+      ];
+
+    beforeEach(module("customers"));
+
+    beforeEach(inject(function ($controller, $rootScope, $httpBackend) {
+      scope = $rootScope.$new();
+      httpBackend = $httpBackend;
+      controller = $controller("CustomerSearchController", {
+        $scope: scope
+      });
+    }));
+
+    beforeEach(function() {
+      httpBackend.when('GET','/customers.json?keywords=bob&page=0')
+        .respond(serverResults);
+    });
+
+    it("populates the customer list with the results", function() {
+      scope.search("bob");
+      httpBackend.flush();
+      expect(scope.customers).toEqualData(serverResults);
+    });
+  });
 });
